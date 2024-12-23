4d50ccc376c7f153cba36c2a45a093606406688f
xmiska03 <zuzkamiskanova@gmail.com>
2024-12-23 12:27:46 +0100

    Add intensities, trasparency and far plane

--- render_pydeck.py
+++ render_pydeck.py
@@ -13,7 +13,7 @@ pc_dict = {
   "x": pc_nparray[:, 0],
   "y": pc_nparray[:, 1],
   "z": pc_nparray[:, 2],
-  "intensity": pc_nparray[:, 3],
+  "r": pc_nparray[:, 3] * 6
 }
 pc_df = pd.DataFrame(pc_dict)
 
@@ -21,7 +21,7 @@ point_cloud_layer = pydeck.Layer(
     "PointCloudLayer",
     data=pc_df,
     get_position=["x", "y", "z"],
-    get_color=["intensity"],
+    get_color=["r", "r", "r", 125],
     get_normal=[0, 0, 0],
     auto_highlight=True,
     pickable=True,
@@ -29,7 +29,7 @@ point_cloud_layer = pydeck.Layer(
 )
 
 view_state = pydeck.ViewState(target=[5.7, -0.08 - 1.3, -0.07], controller=True, rotation_orbit=90, rotation_x=0, zoom=10)
-view = pydeck.View(type="OrbitView", controller=True, near=5, fovy=20)
+view = pydeck.View(type="OrbitView", controller=True, near=5, far=300, fovy=20)
 
 r = pydeck.Deck(point_cloud_layer, initial_view_state=view_state, views=[view])
 r.to_html("output/pc_pydeck.html", css_background_color="#add8e6")
