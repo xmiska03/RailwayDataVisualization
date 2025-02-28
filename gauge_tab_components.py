da28dfe82691223de5e453d2e5b53c37f2883807
xmiska03 <zuzkamiskanova@gmail.com>
2025-02-28 19:47:21 +0100

    Create basic GUI elements for loading gauge manipulation

---
+++ gauge_tab_components.py
@@ -0,0 +1,21 @@
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+
+gauge_tab = [
+    dbc.Row(dcc.Checklist(
+            options=[{'label': ' zobrazovat průjezdný profil', 'value': 'pcl'}],
+            value=['pcl'],
+            id='loading-gauge-checkbox',
+            style={'margin-top':'20px', 'margin-bottom':'20px'}
+        )
+    ),
+    dbc.Row(html.Div("Vzdálenost průjezdného profilu:")),
+    dbc.Row(dcc.Input(
+        value=50,
+        id="gauge-distance-slider-input",
+        type="range",
+        min=20,
+        max=80,
+        style={"margin":"10px", "width": "90%"}
+    ))
+]
