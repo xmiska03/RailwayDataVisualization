2695b0dd78f8060929c325b005d6d225ce43df5d
xmiska03 <zuzkamiskanova@gmail.com>
2025-03-30 13:59:03 +0200

    Add experiment - pre-calculate coordinates for distortion

--- visualization_tab_callbacks.py
+++ visualization_tab_callbacks.py
@@ -187,36 +187,20 @@ def get_callbacks(app):
             const P2 = 0.000804309339521888
             const K3 = 0.3354456739081583
             
-            // the canvas into which deck.gl draws
-            const canvas = document.getElementById("visualization-canvas");
-            const gl_ctx = canvas.getContext("webgl2");
-            
-            // the canvas into which this function will draw the result
-            const dist_canvas = document.getElementById("distorted-visualization-canvas");
-            const dist_ctx = dist_canvas.getContext("2d");
-            
-            const width = canvas.width;
-            const height = canvas.height;
-            dist_canvas.width = width;    // set the same size for the canvas with distorted image
-            dist_canvas.height = height;
-            dist_canvas.style.width = canvas.style.width;
-            dist_canvas.style.height = canvas.style.height;
+            const width = 910;
+            const height = 682;
 
-            //gl_ctx.bindFramebuffer(gl_ctx.FRAMEBUFFER, null); 
-            const pixelData = new Uint8Array(width * height * 4); // RGBA data
-            gl_ctx.readPixels(0, 0, width, height, gl_ctx.RGBA, gl_ctx.UNSIGNED_BYTE, pixelData);
+            console.log(width, height);
 
-            // for every point on the canvas
+            window.dist_array = new Array(height);
+
+            // for every pixel on the canvas
             for (let y = 0; y < height; y++) {
+                
+                window.dist_array[y] = new Array(width);
+                
                 for (let x = 0; x < width; x++) {
-                    
-                    // get pixel color
-                    const r = pixelData[(y * width + x) * 4];
-                    const g = pixelData[(y * width + x) * 4 + 1];
-                    const b = pixelData[(y * width + x) * 4 + 2];
-                    const a = pixelData[(y * width + x) * 4 + 3];
-
-                    // calculate new coordinates for the pixel
+                    // pre-calculate new coordinates for the pixel
 
                     // convert to normalized image coordinates
                     const y_norm = (y - Math.round(height/2)) / width
@@ -238,6 +222,72 @@ def get_callbacks(app):
                     const y_dist = Math.round(y_dist_norm * width + Math.round(height/2))
                     const x_dist = Math.round(x_dist_norm * width + Math.round(width/2))
 
+                    window.dist_array[y][x] = [y_dist, x_dist];
+                }
+            }
+            console.log("distortion pre-calculation took", Date.now() - beg_time, "ms");
+            console.log(window.dist_array);
+            return dash_clientside.no_update;
+        }
+        """,
+        Output('distortion-button', 'id'),  # dummy input and output
+        Input('distortion-button', 'id')
+    )
+
+    # distort the point cloud
+    app.clientside_callback(
+        """
+        function(btn) {
+            const beg_time = Date.now();
+
+            const K1 = -0.1832170424487164 
+            const K2 = 0.02691675026209955
+            const P1 = -0.001191374354805736
+            const P2 = 0.000804309339521888
+            const K3 = 0.3354456739081583
+            
+            // the canvas into which deck.gl draws
+            const canvas = document.getElementById("visualization-canvas");
+            const gl_ctx = canvas.getContext("webgl2");
+            
+            // the canvas into which this function will draw the result
+            const dist_canvas = document.getElementById("distorted-visualization-canvas");
+            const dist_ctx = dist_canvas.getContext("2d");
+            
+            const width = canvas.width;
+            const height = canvas.height;
+
+            console.log(width, height);
+
+            dist_canvas.width = width;    // set the same size for the canvas with distorted image
+            dist_canvas.height = height;
+            dist_canvas.style.width = canvas.style.width;
+            dist_canvas.style.height = canvas.style.height;
+
+            //gl_ctx.bindFramebuffer(gl_ctx.FRAMEBUFFER, null); 
+            const pixelData = new Uint8Array(width * height * 4); // RGBA data
+            gl_ctx.readPixels(0, 0, width, height, gl_ctx.RGBA, gl_ctx.UNSIGNED_BYTE, pixelData);
+
+            // for every point on the canvas
+            for (let y = 0; y < height; y++) {
+                for (let x = 0; x < width; x++) {
+                    
+                    // get pixel color
+                    const r = pixelData[(y * width + x) * 4];
+                    const g = pixelData[(y * width + x) * 4 + 1];
+                    const b = pixelData[(y * width + x) * 4 + 2];
+                    const a = pixelData[(y * width + x) * 4 + 3];
+
+                    // calculate new coordinates for the pixel
+                    
+                    if (a == 0) {
+                        continue;    // empty space, no need to write anything
+                    }
+
+                    // new coordinates for the pixel are pre-calculated
+                    const y_dist = window.dist_array[y][x][0];
+                    const x_dist = window.dist_array[y][x][1];
+
                     // write the pixel on the new position
                     dist_ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${a/255})`;
                     dist_ctx.fillRect(x_dist, (height - 1) - y_dist, 1, 1);
@@ -251,3 +301,4 @@ def get_callbacks(app):
         Input('distortion-button', 'n_clicks'),
         prevent_initial_call=True
     )
+
